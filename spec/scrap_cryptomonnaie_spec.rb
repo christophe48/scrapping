283fac73d300b76c560e30c9f307e819a6eb481a
Christophe BONNEFOY <christophe48bonnefoy@gmail.com>
2019-10-10 17:45:36 +0200

    le spec des crypto en fonction de la taille du tableau

---
+++ spec/scrap_cryptomonnaie_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/scrap_cryptomonnaie.rb'
+
+describe "method_name, it should return a hash with both arrays" do
+ it "size type" do
+   expect(method_name().size).to eq(2399)
+ end
+end
