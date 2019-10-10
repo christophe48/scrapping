6209f926c3dc6d13298559ab6c0ca6d616504f05
Christophe BONNEFOY <christophe48bonnefoy@gmail.com>
2019-10-10 16:40:33 +0200

    Il fallait enlever le $

--- lib/scrap_cryptomonnaie.rb
+++ lib/scrap_cryptomonnaie.rb
@@ -8,11 +8,11 @@ def crypto() # je récupère les noms des cryptos
 	# td sont les colonnes du tableau du site et tr sont les lignes
 	# si je veux la 3 ème colonne je fait //td[3]
 	crypto = page.xpath('//td[3]')
-	return crypto 
+	return crypto
 end
 
 def valeur() # je récupère les valeurs de prix
-	valeur = page.xpath("//a[@class='price']") # Dans le inspec nous pouvons qu'il existe une class 'price' 
+	valeur = page.xpath("//a[@class='price']") # Dans le inspec nous pouvons qu'il existe une class 'price'
 	#le lien /a signifie que les valeurs sont des liens hypertext
 	return valeur
 end
@@ -24,9 +24,11 @@ def method_name()
 	tabl = [] # on créé un tableau vide pour stocker les hash
 	while i < valeur.length
 		nouveauHash = Hash.new()# on créé un nouveau hash à chaque tour de boucle
-		nouveauHash[crypto[i].text] = valeur[i].text # on rentre les valeurs dans le hash
+		nouveauHash[crypto[i].text] = valeur[i].text.delete("$") # on rentre les valeurs dans le hash
 		tabl << nouveauHash # on met chaque hash dans le tableau
 		i+=1
 	end
 	return tabl
 end
+
+puts method_name()
