00afae4b574875c4600306dee2a2989a88edf25d
Ecris ton nom ici, en gardant les quotes <ludovict@gmail.com>
2019-10-10 17:47:40 +0200

    ajout scrap mail

--- lib/scrap_email.rb
+++ lib/scrap_email.rb
@@ -7,33 +7,52 @@ end
 
 #je récupère les villes
 def townhall(page)
-townhall = page.xpath("//a[@class='lientxt']")
-return townhall.text
+  townhall = page.xpath("//a[@class='lientxt']/text()")
+  return townhall
 end
 
 #je récupère les URL par villes
 def get_townhall_urls(page)
   url = page.xpath("//a[@class='lientxt']")
+  tableau = []
+  url.each do |townhall_url|
+      tableau << "http://annuaire-des-mairies.com/" + townhall_url['href']
+  end
 
-tableau = []
-
-url.each do |townhall_url|
-        tableau << "http://annuaire-des-mairies.com/" + townhall_url['href']
-   end
-   return tableau #je met tout les mail dans un tableau
+  return tableau #je met tout les mail dans un tableau
 end
 
 #je récupère les mails des villes
-def get_townhall_email(tableau)
+def get_townhall_email()
   i=0
-  while i<tableau.size
-  Nokogiri::HTML(open(tableau[i]))
-  townhall_email = page.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]")
-  i += 1
- end
-return townhall_email
+  table = get_townhall_urls(page)
+  townhall_email = []
+  while i < table.length
+    page2 = Nokogiri::HTML(open(table[i]))# ouverture d'une nouvelle page pour chaque élément du tableau
+    townhall_email << page2.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]/text()")
+    i += 1
+  end
+  return townhall_email
 end
 
+
+def make_hashe(townhall, get_townhall_email)# je créé un hash alimentant un tableau
+  i = 0
+  #email = get_townhall_email()
+  #nom_ville = townhall(page)
+  tableau_ville_mail = []
+  while i < get_townhall_email.length
+    nouveau_hash = Hash.new()
+    nouveau_hash[townhall[i].text] = get_townhall_email[i].text
+    tableau_ville_mail << nouveau_hash
+    i += 1
+  end
+  return tableau_ville_mail
+end
+
+puts make_hashe(townhall(page), get_townhall_email)
 #townhall_url
 #get_townhall_urls(page)
-puts get_townhall_email(get_townhall_urls(page))
+#puts get_townhall_email()
+#puts make_hashe()
+#puts townhall(page)
