8d156496a2f2cd8a04810e88864b587ddca8f636
Ecris ton nom ici, en gardant les quotes <ludovict@gmail.com>
2019-10-10 23:18:04 +0200

    modif

--- lib/scrap_deputes.rb
+++ lib/scrap_deputes.rb
@@ -24,9 +24,9 @@ def get_deputes_email()
   i = 0
   table = get_deputes_urls(page)
   deputes_email = []
-  while i < table.length
+  while i < table.length # mettre un valeur 10 par exemple pour faire le test
     page2 = Nokogiri::HTML(open(table[i]))# ouverture d'une nouvelle page pour chaque élément du tableau
- deputes_email << page2.xpath("//*[@id='haut-contenu-page']/article/div[3]/div/dl/dd[4]/ul/li[2]/a/text()")
+ 	deputes_email << page2.xpath("//*[@id='haut-contenu-page']/article/div[3]/div/dl/dd[4]/ul/li[2]/a/text()")
     i += 1
   end
   return deputes_email
@@ -38,7 +38,7 @@ def make_hashe()# je créé un hash alimentant un tableau
   nom_depute = deputes(page)
   tableau_depute_mail = []
 
-  while i < nom_depute.length
+  while i < nom_depute.length # mettre un valeur 10 par exemple pour faire le test
     nouveau_hash = Hash.new()
     nouveau_hash[nom_depute[i].text] = email[i].text
     tableau_depute_mail << nouveau_hash
